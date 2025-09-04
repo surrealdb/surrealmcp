e2589980f15ec21759ca03a7bcb490d4bb188160
Martin Schaer <martin.schaer@surrealdb.com>
2025-09-04 18:12:52 +0100

    install default ring provider

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ mod utils;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+        println!("Failed to install default crypto provider: {:?}", e);
+    }
+
     // Parse command line arguments
     let cli = cli::Cli::parse();
     // Run the specified command
